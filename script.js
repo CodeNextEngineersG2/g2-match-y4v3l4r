9375eee84a6ebc363f6f9d6f2a197c4e16b8a0c0
Student <student@Admins-MacBook-Air.local>
2018-04-28 12:56:12 -0700

    update

--- script.js
+++ script.js
@@ -42,18 +42,19 @@ var resetButton, musicButton;
      myImage = loadImage("assets/img/image.png");
    }
  */
-function loadImages() {
-  backImage = loadImage("assests/img/back.png");
-  boltImage = loadImage("assests/img/bolt.png");
-  cloudImage = loadImage("assests/img/cloud.png");
-  sunImage = loadImage ("assests/img/sun.png");
-  moonImage = loadImage("assests/img/moon.png");
-  smileyImage = loadImage("assests/img/smiley.png");
-  heartImage = loadImage("assests/img/heart.png");
-  transitionImage1 = loadImage("assests/img/transition1.png");
-  transitionImage2 = loadImage("assests/img/transition2.png");
-  transitionImage3 = loadImage("assests/img/transition3.png");
-}
+ function loadImages() {
+   backImage = loadImage("assets/img/back.png");
+   boltImage = loadImage("assets/img/bolt.png");
+   cloudImage = loadImage("assets/img/cloud.png");
+   sunImage = loadImage("assets/img/sun.png");
+   moonImage = loadImage("assets/img/moon.png");
+   smileyImage = loadImage("assets/img/smiley.png");
+   heartImage = loadImage("assets/img/heart.png");
+   transitionImage1 = loadImage("assets/img/transition1.png");
+   transitionImage2 = loadImage("assets/img/transition2.png");
+   transitionImage3 = loadImage("assets/img/transition3.png");
+ }
+
 
 /*
  * function loadAnimations()
@@ -66,15 +67,15 @@ function loadImages() {
      myAnimation = loadAnimation(img1, img2, img3, img4);
    }
  */
-
-function loadAnimations(){
+function loadAnimations() {
   boltAnimation = loadAnimation(backImage, transitionImage1, transitionImage2, transitionImage3, boltImage);
   cloudAnimation = loadAnimation(backImage, transitionImage1, transitionImage2, transitionImage3, cloudImage);
   sunAnimation = loadAnimation(backImage, transitionImage1, transitionImage2, transitionImage3, sunImage);
   moonAnimation = loadAnimation(backImage, transitionImage1, transitionImage2, transitionImage3, moonImage);
-  smileyAnimation = loadAnimation(backImage, transitionImage1, transitionImage2, transitionImage3,smileyImage);
+  smileyAnimation = loadAnimation(backImage, transitionImage1, transitionImage2, transitionImage3, smileyImage);
   heartAnimation = loadAnimation(backImage, transitionImage1, transitionImage2, transitionImage3, heartImage);
 }
+
 /*
  * function loadSounds()
  * Works very similarly to loadImages(), only for music and sound effects.
@@ -94,10 +95,9 @@ function loadAnimations(){
  * begin running until the assets are loaded and ready. Therefore, this function
  * is essentially a "pre-setup" function.
  */
-
-function preload(){
-loadImages();
-loadAnimations();
+function preload() {
+  loadImages();
+  loadAnimations();
 }
 
 /*
@@ -106,21 +106,34 @@ loadAnimations();
  * Therefore, assets are assumed to have been loaded and ready before this
  * function is called.
  */
-function setup() {
-  gameScreen = createCanvas(790, 370);
-  gameScreen.parent("#game-screen");
-  spriteWidth = 120;
-  spriteHeight = 168;
-  spriteX = 70;
-  spriteY = 95;
-  imageArray = [backImage, boltImage, cloudImage, sunImage, moonImage, smileyImage, heartImage,
-                transitionImage1, transitionImage2, transitionImage3];
-resizeImages();
-}
+ function setup() {
+   gameScreen = createCanvas(790, 370);
+   gameScreen.parent("#game-screen"); // Just like maurry
+   spriteWidth = 120;
+   spriteHeight = 168;
+   spriteX = 70;
+   spriteY = 95;
+   imageArray = [backImage, boltImage, cloudImage, sunImage, moonImage, smileyImage, heartImage,
+                 transitionImage1, transitionImage2, transitionImage3];
+   resizeImages();
+   createSprites();
+   spriteArray = [boltSprite1, boltSprite2, cloudSprite1, cloudSprite2,
+     sunSprite1, sunSprite2, moonSprite1, moonSprite2, smileySprite1, smileySprite2,
+     heartSprite1, heartSprite2];
+   addAnimations();
+   shuffle(spriteArray, true);
+   placeSprites();
+   spritesActive = true;
+ }
+
 
 /*
  * function draw()
  */
+ function draw() {
+   background(20, 40, 60);
+   drawSprites();
+ }
 
 /*
  * function init()
@@ -171,6 +184,21 @@ function resizeImages() {
    }
  */
 
+ function createSprites() {
+   boltSprite1 = createSprite(0, 0, spriteWidth, spriteHeight);
+   boltSprite2 = createSprite(0, 0, spriteWidth, spriteHeight);
+   cloudSprite1 = createSprite(0, 0, spriteWidth, spriteHeight);
+   cloudSprite2 = createSprite(0, 0, spriteWidth, spriteHeight);
+   sunSprite1 = createSprite(0, 0, spriteWidth, spriteHeight);
+   sunSprite2 = createSprite(0, 0, spriteWidth, spriteHeight);
+   moonSprite1 = createSprite(0, 0, spriteWidth, spriteHeight);
+   moonSprite2 = createSprite(0, 0, spriteWidth, spriteHeight);
+   smileySprite1 = createSprite(0, 0, spriteWidth, spriteHeight);
+   smileySprite2 = createSprite(0, 0, spriteWidth, spriteHeight);
+   heartSprite1 = createSprite(0, 0, spriteWidth, spriteHeight);
+   heartSprite2 = createSprite(0, 0, spriteWidth, spriteHeight);
+ }
+
 
 /*
  * function addAnimations()
@@ -180,7 +208,20 @@ function resizeImages() {
  * each animation's frameDelay, loop, and playing properties. Finally, this
  * function calls activateSprite(s) with each sprite as input.
  */
-
+ function addAnimations() {
+   console.log("Did I even reach this part? Add animations?");
+   var animations = [boltAnimation, boltAnimation, cloudAnimation, cloudAnimation,
+                    sunAnimation, sunAnimation, moonAnimation, moonAnimation,
+                    smileyAnimation, smileyAnimation,
+                    heartAnimation, heartAnimation];
+   for(var i = 0; i < spriteArray.length; i++) {
+     spriteArray[i].addAnimation("flip", animations[i]);
+     spriteArray[i].animation.frameDelay = 10;
+     spriteArray[i].animation.looping = false;
+     spriteArray[i].animation.playing = false;
+     activateSprite(spriteArray[i]);
+   }
+ }
 
 /*
  * function placeSprites()
@@ -188,6 +229,20 @@ function resizeImages() {
  * pattern you like. For starters, try arranging the sprites in a simple
  * grid-like pattern (e.g., 2x2 if you only have four sprites).
  */
+ function placeSprites() {
+   for(var i = 0; i < spriteArray.length; i++) {
+     spriteArray[i].position.x = spriteX;
+     spriteArray[i].position.y = spriteY;
+     if((i + 1) % 6 === 0) { // if the number of sprites is divisible by 6
+       spriteX = 70;
+       spriteY += spriteHeight + 10;
+     }
+     else {
+       spriteX += spriteWidth + 10;
+     }
+   }
+ }
+
 
 
 /*
@@ -202,7 +257,24 @@ function resizeImages() {
  * two sprites have been clicked, the function calls checkMatch().
  */
 
+//funactions are hoiseted to the top of thr script ; variables are not hoiseted.
 
+
+function activateSprite(s){
+  s.onMousePressed = function(){
+    if(spritesActive && s.animation.getFrame() !== s.animation.getLastFrame()){
+      if(firstsprite === undefined){
+      firstsprite = s;
+      //flipSound.play();
+      s.animation.goToFrame(s.animation.getLastFrame());
+      }
+      else if(s !== firstsprite)
+      secondsprite = s;
+      //flipSound.play();
+      s.animation.goToFrame(s.animation.getLastFrame());
+    }
+  }
+}
 
 /*
  * function checkMatch()
